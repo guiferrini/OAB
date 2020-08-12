9e624fe53a31d3d23679578d802cab33d28930e6
Guilherme Ferrini <guilhermemferrini@gmail.com>
2020-08-12 16:49:39 -0300

    build

---
+++ web/build/precache-manifest.ee8011044730f841f924ba1fec9d2371.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5f8ea61a6f1ec0046471f4cb96dddf8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57fd231a70d405f287eb",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8669300c3e43a040d021",
+    "url": "/static/css/main.f04444c5.chunk.css"
+  },
+  {
+    "revision": "57fd231a70d405f287eb",
+    "url": "/static/js/2.20faadb6.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/static/js/2.20faadb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8669300c3e43a040d021",
+    "url": "/static/js/main.2e872ec1.chunk.js"
+  },
+  {
+    "revision": "d0a598c2c4f910599d51",
+    "url": "/static/js/runtime-main.e7310a31.js"
+  },
+  {
+    "revision": "2b17836f3b16d1c7d1b2fdc7b37830df",
+    "url": "/static/media/Roberto_Nascimento_inteira.2b17836f.png"
+  },
+  {
+    "revision": "b5ac88ee9e8bba11beb2265cadbb2bcb",
+    "url": "/static/media/garantia_oab.b5ac88ee.png"
+  },
+  {
+    "revision": "29bcf1403c6fd178cd2d35de8ee3b0db",
+    "url": "/static/media/logosetesete.29bcf140.png"
+  },
+  {
+    "revision": "897c7851b83583949d482f4b532cbf82",
+    "url": "/static/media/oab_central.897c7851.png"
+  },
+  {
+    "revision": "368557de97663e13a3a939d378dc4afe",
+    "url": "/static/media/oab_faixa.368557de.png"
+  },
+  {
+    "revision": "ffcb95cfdb18331daad64cb047fbfe2a",
+    "url": "/static/media/oferta.ffcb95cf.png"
+  }
+]);
